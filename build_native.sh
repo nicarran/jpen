0b676a7d8d44c9843b810cd2b3cf4af3a5e044ba
Brien Colwell <xcolwell@gmail.com>
2008-06-11 07:54:55 +0000

    git-svn-id: svn://svn.code.sf.net/p/jpen/code/branches/bridge_trunk@46 5c467ca5-5641-0410-8515-9841fa1a992e

--- build_native.sh
+++ build_native.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 ./build_setup.sh
-# ./build_win.sh
+ ./build_win.sh
 # ./build_linux.sh
-./build_osx.sh
+# ./build_osx.sh
 
 # Update the pre-packaged dist:
 cp dist/jpen-2.* prepackaged_hack
-
