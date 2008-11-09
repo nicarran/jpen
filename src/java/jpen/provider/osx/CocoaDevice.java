6aa84df9f34f3f276bd177dd9684fef15872357e
Nicolas Carranza <nicarran@gmail.com>
2008-11-09 20:24:27 +0000

    * Copied current osx provider implementation from branches/bridge_trunk to trunk.
    git-svn-id: svn://svn.code.sf.net/p/jpen/code/trunk@69 5c467ca5-5641-0410-8515-9841fa1a992e

---
+++ src/java/jpen/provider/osx/CocoaDevice.java
@@ -0,0 +1,27 @@
+package jpen.provider.osx;
+
+import jpen.PKind;
+import jpen.provider.AbstractPenDevice;
+
+public class CocoaDevice extends AbstractPenDevice {
+	private final PKind.Type type;
+	
+	public CocoaDevice(final CocoaProvider _cocoaProvider, final PKind.Type _type) {
+		super(_cocoaProvider);
+		
+		type = _type;
+		
+		setKindTypeNumber(type.ordinal());
+	}
+	
+	
+	public String getName() {
+		switch (type) {
+			case CURSOR: return "Cocoa Cursor";
+			case ERASER: return "Cocoa Eraser";
+			case STYLUS: return "Cocoa Stylus";
+			default:
+				return "UNKNOWN";
+		}
+	}
+}
---
+++ src/java/jpen/provider/osx/CocoaProvider.java
@@ -0,0 +1,75 @@
+package jpen.provider.osx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jpen.PKind;
+import jpen.PenManager;
+import jpen.PenProvider;
+import jpen.provider.AbstractPenProvider;
+import jpen.provider.Utils;
+
+public class CocoaProvider extends AbstractPenProvider {
+	public static class Constructor implements PenProvider.Constructor {
+		public Constructor() {
+		}
+		
+		public String getName() {
+			return "Cocoa";
+		}
+		
+		public boolean constructable() {
+			return System.getProperty("os.name").toLowerCase().contains("mac");
+		}
+	
+		public PenProvider construct(PenManager pm) throws ConstructionException {
+			try {
+				Utils.loadLibrary();
+				CocoaAccess cocoaAccess=new CocoaAccess();
+				return new CocoaProvider(pm, this, cocoaAccess);
+			} catch(Throwable t) {
+				throw new ConstructionException(t);
+			}
+		}
+	}
+
+	
+	private final CocoaAccess cocoaAccess;
+	private final Map<PKind.Type, CocoaDevice> deviceMap;
+	
+	private CocoaProvider(final PenManager _penManager, 
+			final Constructor _constructor, final CocoaAccess _cocoaAccess
+	) {
+		super(_penManager, _constructor);
+		cocoaAccess = _cocoaAccess;
+		cocoaAccess.setProvider(this);
+		
+		deviceMap = new HashMap<PKind.Type, CocoaDevice>(3);
+		for (PKind.Type type : PKind.Type.VALUES) {
+			final CocoaDevice device = new CocoaDevice(this, type);
+			deviceMap.put(type, device);
+			getPenManager().firePenDeviceAdded(getConstructor(), device);
+		}
+		
+		
+		cocoaAccess.start();
+	}
+	
+	public CocoaDevice getDevice(final PKind.Type type) {
+		return deviceMap.get(type);
+	}
+	
+	public void penManagerPaused(final boolean paused) {
+		if (! paused) {
+			cocoaAccess.enable();
+		}
+		else {
+			cocoaAccess.disable();
+		}
+	}
+	
+	
+	public void dispose() {
+		cocoaAccess.stop();
+	}
+}
