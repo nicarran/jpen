a1dc0041f4b3e6907d7010955a18115580f796ec
Nicolas Carranza <nicarran@gmail.com>
2008-11-11 03:44:41 +0000

    * Changed copyright/license header to allow better maintenance. See http://www.softwarefreedom.org/resources/2007/gpl-non-gpl-collaboration.html .
    git-svn-id: svn://svn.code.sf.net/p/jpen/code/trunk@71 5c467ca5-5641-0410-8515-9841fa1a992e

--- src/java/jpen/provider/wintab/WintabAccess.java
+++ src/java/jpen/provider/wintab/WintabAccess.java
@@ -1,23 +1,21 @@
 /* [{
-* (C) Copyright 2007 Nicolas Carranza and individual contributors.
-* See the jpen-copyright.txt file in the jpen distribution for a full
-* listing of individual contributors.
-*
-* This file is part of jpen.
-*
-* jpen is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* jpen is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with jpen.  If not, see <http://www.gnu.org/licenses/>.
-* }] */
+Copyright 2007, 2008 Nicolas Carranza <nicarran at gmail.com>
+
+This file is part of jpen.
+
+jpen is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+jpen is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with jpen.  If not, see <http://www.gnu.org/licenses/>.
+}] */
 package jpen.provider.wintab;
 
 import java.util.Arrays;
