aa48a816331e312cfe8ef0ec2c30b3a98ffd2da0
Nicolas Carranza <nicarran@gmail.com>
2008-12-19 06:40:34 +0000

    * Changed SmokeTest. See http://sourceforge.net/forum/message.php?msg_id=5803552 .
    git-svn-id: svn://svn.code.sf.net/p/jpen/code/trunk@116 5c467ca5-5641-0410-8515-9841fa1a992e

--- src/test/java/jpen/test/SmokeTest.java
+++ src/test/java/jpen/test/SmokeTest.java
@@ -1,35 +1,25 @@
 package jpen.test;
 
-import junit.framework.*;
-import jpen.*;
-import javax.swing.*;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import jpen.PenManager;
+import jpen.PenProvider;
+import junit.framework.TestCase;
 
 public class SmokeTest extends TestCase {
 
-    public SmokeTest(String name) {
-      super(name);
-    }
-
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
-
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-    public void testPenManager() throws Exception {
-        JComponent component = new JPanel();
-        PenManager penManager = new PenManager(component);
-        System.out.println("Providers:");
-        int count=0;
-        for(PenProvider.Constructor constructor: penManager.getConstructors()){
-            if(!constructor.constructable())
-                continue;
-            count++;
-            System.out.println("Constructor: "+constructor.getName());
-        }
-        assertTrue(count>=2);
-    }
+	public void testPenManager() throws Exception {
+		JComponent component = new JPanel();
+		PenManager penManager = new PenManager(component);
+		System.out.println("Providers:");
+		for(PenProvider.Constructor constructor: penManager.getConstructors()){
+			System.out.println("Constructor: "+constructor.getName());
+			PenProvider.ConstructionException ex=constructor.getConstructionException();
+			if(ex!=null){
+				ex.printStackTrace();
+				fail(ex.getMessage());
+			}
+		}
+	}
+	
 }
-
