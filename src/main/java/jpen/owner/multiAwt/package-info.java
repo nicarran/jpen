5d7b41acf06057ea9ad47e78c4629978934c02e4
Nicolas Carranza <nicarran@gmail.com>
2011-09-26 17:58:44 +0000

    * javadoc
    git-svn-id: svn://svn.code.sf.net/p/jpen/code/trunk@281 5c467ca5-5641-0410-8515-9841fa1a992e

---
+++ src/main/java/jpen/owner/multiAwt/package-info.java
@@ -0,0 +1,22 @@
+/* [{
+Copyright 2010 Nicolas Carranza <nicarran at gmail.com>
+
+This file is part of jpen.
+
+jpen is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+jpen is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with jpen.  If not, see <http://www.gnu.org/licenses/>.
+}] */
+/**
+Contains {@link jpen.owner.multiAwt.AwtPenToolkit} which allows JPen to be used on one or many Java AWT components. 
+*/
+package jpen.owner.multiAwt;
